62db381858d73d5e3a3a6a95bc5ea4ca5d6ff515
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-11-12 17:52:43 -0600

    added ability to handle pagination on tapspace

--- index.py
+++ index.py
@@ -1,18 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://www.tapspace.com/marimba-xylophone/')
-soup = BeautifulSoup(r.text, features="html.parser")
-cards = soup.findAll('div', class_='catalog-list2')
 
-# https://www.tapspace.com/index.php?p=catalog&parent=114&pg=5&ajax=true&mode=catalog&selected_filters=&CatalogSetSortBy=priority&action_relaod=1
+def get_title(card):
+    title_info = card.find('div', class_='catalog-product-title')
+    return title_info.a.contents[0]
 
-solo_data = []
+page = 1
+mallet_solo_data = []
 
-for card in cards:
-    title_info = card.find('div', class_="catalog-product-title")
-    data = { 'title': title_info.a.contents[0] }
-    solo_data.append(data)
-	  
-print(solo_data)
-  
+while True:
+    r = requests.get(
+        f"https://www.tapspace.com/index.php?p=catalog&parent=114&pg={page}&ajax=true&mode=catalog&selected_filters=&CatalogSetSortBy=priority&action_relaod=1'")
+
+    if r.status_code != 200:
+        break
+
+    soup = BeautifulSoup(r.text, features="html.parser")
+    cards = soup.findAll('div', class_="catalog-list2")
+
+    for card in cards:
+        title = get_title(card)
+        mallet_solo_data.append({"title": title})
+
+    page += 1
+
+print(mallet_solo_data)
+print(len(mallet_solo_data))
+
